b5f7a7660984f34bc94517593dae8bc544df1937
kmsaigon <kevinb0919@gmail.com>
2025-09-25 03:33:41 -0400

    updated templates and project configuration for improved structure
    - Enhanced `base.html` with improved navigation and footer design.
    - Updated `about.html` and `index.html` for clearer messaging and layout.
    - Modified `urls.py` to ensure correct routing for home and about pages.
    - Updated project settings and WSGI/ASGI files for consistency.
    - General cleanup and alignment with Django best practices.

--- jobsearchplatform/asgi.py
+++ jobsearchplatform/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for moviesstore project.
+ASGI config for jobsearchplatform project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moviesstore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobsearchplatform.settings')
 
 application = get_asgi_application()
